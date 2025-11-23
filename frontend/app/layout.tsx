50bb81fa53d86952370fe799a8e797056f7ee887
miguelrsant <miguelrsant01@gmail.com>
2025-11-23 05:02:06 -0300

    refactor: improve home page layout and UX

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,6 @@
-"use client"; 
-
 import type { Metadata } from "next";
 
 import '../public/styles/index.css' 
-import React, { useEffect } from 'react';
 
 const metadata: Metadata = {
   title: "Taylor: Business Super Intelligence Platform",
@@ -15,27 +12,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      const cursorOuter = document.querySelector('.cursor-outer') as HTMLElement | null;
-      const cursorDot = document.querySelector('.cursor-dot') as HTMLElement | null;
-
-      if (cursorDot && cursorOuter) {
-        cursorDot.style.left = e.clientX + 'px';
-        cursorDot.style.top = e.clientY + 'px';
-
-        cursorOuter.style.left = e.clientX + 'px';
-        cursorOuter.style.top = e.clientY + 'px';
-      }
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
 
   return (
     <html lang="pt-br"
@@ -44,8 +20,6 @@ export default function RootLayout({
         <link rel="shortcut icon" href="./svg/taylor-logo-gradiente.svg"/>
       </head>
       <body>
-        <div className="cursor-outer"></div>
-        <div className="cursor-dot"></div>
         {children}
       </body>
     </html>
